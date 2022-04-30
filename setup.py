70ed00a7e9544ac768fcac9e27210cb15a7d7660
Carlos Andrés Planchón Prestes <carlosandresplanchonprestes@gmail.com>
2022-04-30 16:06:37 -0300

    Updating license to MIT.

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
 from pathlib import Path
 
+from setuptools import setup
 
 readme = Path("README.md").read_text()
 
 setup(
     name="checkcorruptedimages",
     packages=["checkcorruptedimages"],
-    version="0.2",
-    license="GPL3",
+    version="0.3",
+    license="MIT",
     description="Python3 module to check for corrupted images using "
             '"identify" from ImageMagick as underlying mechanism.',
     long_description=readme,
@@ -19,16 +19,12 @@ setup(
     author_email="bubbledoloresuruguay2@gmail.com",
     url="https://github.com/carlosplanchon/checkcorruptedimages",
     download_url="https://github.com/carlosplanchon/"
-        "checkcorruptedimages/archive/v0.2.tar.gz",
+        "checkcorruptedimages/archive/v0.3.tar.gz",
     keywords=["check", "corrupted", "images"],
     classifiers=[
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
     ],
 )
